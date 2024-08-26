c7ac8fb0bae6076d7ec3af771d17f666889f4669
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2024-08-26 02:17:14 +0200

    fix: only disassemble script from cached data

--- v8-frida/agent/index.ts
+++ v8-frida/agent/index.ts
@@ -8,16 +8,25 @@ const std_cout = Process.mainModule
 
 Interceptor.attach(CodeSerializer.FinalizeDeserializationPtr, {
   onEnter(args) {
+    // Ignore disassembler.js and a bunch of other node built-in modules
+    if (!isCachedDataScript(this.context)) return;
+
     const isolate = args[0];
     const script = new SharedFunctionInfo(args[1]).script();
     const scriptIterator = new ScriptIterator(isolate, script);
 
     for (const sfi of scriptIterator) {
-      if (!sfi.HasBytecodeArray()) continue;
-
       sfi.SharedFunctionInfoPrint(std_cout);
-      const bytecodeArray = sfi.GetActiveByteCodeArray();
-      bytecodeArray.Disassemble(std_cout);
+      sfi.GetActiveByteCodeArray().Disassemble(std_cout);
     }
   },
 });
+
+function isCachedDataScript(context: CpuContext) {
+  const backtrace = Thread.backtrace(context, Backtracer.ACCURATE);
+  return backtrace.some((address) =>
+    DebugSymbol.fromAddress(address).name?.includes(
+      "GetSharedFunctionInfoForScriptWithCachedData"
+    )
+  );
+}
